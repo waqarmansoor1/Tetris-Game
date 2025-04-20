caff9ca34a0fb8ca37f5ad0c4e34d50ae553abfe
Shaikh Waqar Mansoor <42179513+waqarmansoor1@users.noreply.github.com>
2025-04-20 23:38:27 +0500

    Create color.h

---
+++ color.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "raylib.h"
+
+extern Color darkGray;
+extern Color lightGray;
+extern Color red;
+extern Color green;
+extern Color blue;
+extern Color orange;
+extern Color yellow;
+extern Color purple;
+extern Color cyan;
